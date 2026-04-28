e4dffaf583378120bac6225006d712a924bdfb56
s.afraz <s.afraz@rayanbourse.ir>
2026-04-28 14:48:04 +0330

    init project and prepare layout

---
+++ Resume.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Resume.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Resume.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
